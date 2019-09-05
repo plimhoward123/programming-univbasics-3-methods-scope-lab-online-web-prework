c223a5afd8f6d3785c6f80f9e9b8e99ce574ccc0
Plimon Howard V <plimon.howard@gmail.com>
2019-09-05 18:07:47 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -1,9 +1,15 @@
 def mario
   phrase = "It's-a me, Mario!"
   status = 'Thank You Mario! But Our Princess Is In Another Castle!'
+  
   puts phrase
 end
 
 def toadstool
   puts status
+end
+
+def link
+  phrase = "It's Dangerous To Go Alone! Take This."
+  puts phrase 
 end
